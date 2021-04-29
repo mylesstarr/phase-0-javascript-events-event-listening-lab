bb0083295fc76307b5baa8ce203c57fb1b9d21dd
Myles Starr <mylesstarr@gmail.com>
2021-04-29 15:28:31 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
+
+
+// function called "addingEventListner" takes an event as a variable. This vabiable is simply the event that will always happen to input becuase input is in the body of our code and is connected to the built in function "addEventListener" this also takes the event function, and a function() HERE this function is BUILTIN in line and is 
+// function() {alert('I was clicked!')}
+
 const input = document.getElementById('input');
 
-function addingEventListener(event, addEventListener) {
-    
-input.addEventListener(event, function() {
-  alert('I was clicked!');
-}); 
-
+function addingEventListener(event){ 
+  input.addEventListener(event, function() {
+    alert('I was clicked!')
+    }
+  ) 
 }
 
-addingEventListener("keydown")
+
+
+
+
+
+
+
+
+
+
+
+
+// input.addEventListenerToThisFunction("click", ()=> console.log("event")) This is just an example I wrote about adding an event lister
